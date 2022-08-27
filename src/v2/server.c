607fb74651f309c79e90ec3f40360802ef873d03
Denis Bolshakov <Dysprosium92@ya.ru>
2022-08-27 14:28:07 +0300

    add function open_connect

--- src/v2/server.c
+++ src/v2/server.c
@@ -1,27 +1,38 @@
 #include "wrappers.h"
 
-int connect_server(char *hostname, int port) {
+int open_connect(char *port) {
   struct addrinfo hints, *settings, *setting;
+  int optval = 1;
 
   memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
   hints.ai_socktype = SOCK_STREAM; // TCP
   hints.ai_flags = AI_PASSIVE; // write my IP
   hints.ai_flags |= AI_NUMERICSERV; // port is number
+  hints.ai_flags |= AI_ADDRCONFIG;
 
   Getaddrinfo(hostname, port, &hints, &settings);
-  int serverfd;
+  int server_fd;
   for (setting = settings; setting; setting = setting->ai_next) {
-    if ((serverfd = socket(setting->ai_family, setting->ai_socktype,
+    if ((server_fd = socket(setting->ai_family, setting->ai_socktype,
                            setting->ai_protocol) < 0))
       continue;
-    if (connect(serverfd, setting->ai_addr, setting->ai_addrlen) == 0)
-      break;
-    close();
+  
+    Setsockopt((server_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+    
+    if (bind(server_fd, setting->ai_addr, setting->ai_addrlen) == 0)
+               break; 
+    else
+       close(server_fd); 
   }
+               
   Freeaddrinfo(settings);
+               
   if (!setting)
     return -1;
-  else
-    return serverfd;
+  
+ if (listen(server_fd, LISTENQ) < 0) {
+   close(listenfd);
+   return -1; }
+return server_fd;
+  
 }
