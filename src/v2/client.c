9ca680e4e45d9a053c8637bbb94fbb1828ab3e96
dosart <Dysprosium92@ya.ru>
2022-08-30 19:43:46 +0300

    change server. A little stylistic changes

--- src/v2/client.c
+++ src/v2/client.c
@@ -1,41 +1,5 @@
 #include "wrappers.h"
 
-int connect_server(char *hostname, int port) {
-  struct addrinfo hints, *settings, *setting;
+int connect_server(char *hostname, int port) { return 0; }
 
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_UNSPEC; // IPv4 or IPv6
-  hints.ai_socktype = SOCK_STREAM; // TCP
-  hints.ai_flags = AI_PASSIVE; // write my IP
-  hints.ai_flags |= AI_NUMERICSERV; // port is number
-
-  Getaddrinfo(hostname, port, &hints, &settings);
-  int client_fd;
-  for (setting = settings; setting; setting = setting->ai_next) {
-    if ((client_fd = socket(setting->ai_family, setting->ai_socktype,
-                            setting->ai_protocol) < 0))
-      continue;
-    if (connect(client_fd, setting->ai_addr, setting->ai_addrlen) == 0)
-      break;
-    close();
-  }
-  if (!setting)
-    return -1;
-  else
-    return client_fd;
-}
-
-int main() {
-  int client_fd = connect_server("127.0.0.1", "8080");
-
-  char *message = "Hello from client";
-  send(client_fd, message, strlen(message), 0);
-
-  char buffer[2048] = {0};
-  recv(client_fd, buffer, 2048, 0);
-
-  printf("Message from server: %s\n", buffer);
-
-  close(client_fd);
-  return 0;
-}
+int main() { return 0; }
--- src/v2/server.c
+++ src/v2/server.c
@@ -5,36 +5,39 @@
 #include <unistd.h>
 
 #define LISTENQ 10
-#define MAXLINE 2048
+#define HOSTNAME_SIZE 2048
+#define PORT_SIZE 64
 
 int open_listen_fd(char *port) {
 
   struct addrinfo hints, *results, *p;
 
   memset(&hints, 0, sizeof(struct addrinfo));
-  hints.ai_socktype = SOCK_STREAM; //TCP socket
-  hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG; // Write my IP and use IPv4 or IPv6
+  hints.ai_socktype = SOCK_STREAM; // TCP socket
+  hints.ai_flags =
+      AI_PASSIVE | AI_ADDRCONFIG;   // Write my IP and use IPv4 or IPv6
   hints.ai_flags |= AI_NUMERICSERV; // Port is number
-  
+
   int listen_fd, optval = 1;
-  
-  //Find socket using hints. add socket to list of results
+
+  // Find socket using hints. add socket to list of results
   Getaddrinfo(NULL, port, &hints, &results);
   for (p = results; p != NULL; p = p->ai_next) {
     if ((listen_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
       continue; // Bad luck. Try next address
 
     /* Prevent "Address already in use" error */
-    Setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,(const void *)&optval, sizeof(int));
+    Setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval,
+               sizeof(int));
 
-    if (bind(listenfd, p->ai_addr, p->ai_addrlen) == 0)
-      break; // It's luck. 
+    if (bind(listen_fd, p->ai_addr, p->ai_addrlen) == 0)
+      break; // It's luck.
 
     close(listen_fd);
   }
   Freeaddrinfo(results);
 
-  if (p == NULL) //All address is wrong
+  if (p == NULL) // All address is wrong
     return -1;
 
   if (listen(listen_fd, LISTENQ) < 0) {
@@ -49,17 +52,14 @@ int main() {
   int listenfd, connfd;
 
   socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
 
-  struct sockaddr_storage clientaddr; /* Достаточно места для любого адреса */
-
-  char client_hostname[MAXLINE], client_port[MAXLINE];
+  char client_hostname[HOSTNAME_SIZE], client_port[PORT_SIZE];
   char *message_to_client = "HTTP/1.1 200 OK\nContent-Type: "
                             "text/plain\nContent-Length: 12\n\nHello world!";
 
   listenfd = open_listen_fd("29008");
-
   while (1) {
-
     printf("Server waiting\n\n");
     clientlen = sizeof(struct sockaddr_storage);
 
@@ -70,10 +70,7 @@ int main() {
     }
 
     Getnameinfo((struct sockaddr *)&clientaddr, clientlen, client_hostname,
-                MAXLINE,
-
-                client_port, MAXLINE, 0);
-
+                HOSTNAME_SIZE, client_port, PORT_SIZE, 0);
     printf("Connected to (%s, %s)\n", client_hostname, client_port);
 
     char buffer[30000] = {0};
@@ -81,5 +78,5 @@ int main() {
     printf("%s\n", buffer);
     write(connfd, message_to_client, strlen(message_to_client));
   }
-  exit(0);
+  return 0;
 }
