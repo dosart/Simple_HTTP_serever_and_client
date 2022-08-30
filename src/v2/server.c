a3f956f56b7367d4245ebfea1f346ddaf3e992da
Denis Bolshakov <Dysprosium92@ya.ru>
2022-08-30 07:18:15 +0300

    bug fix. server is done

--- src/v2/server.c
+++ src/v2/server.c
@@ -1,42 +1,112 @@
 #include "wrappers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define LISTENQ 10
+#define MAXLINE 2048
 
 int open_listen_fd(char *port) {
 
-  struct addrinfo hints = {0}, *addrs;
-  hints.ai_family = AF_UNSPEC;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_protocol = IPPROTO_TCP;
-  hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
-  hints.ai_flags |= AI_NUMERICSERV;
-  
-  Getaddrinfo(NULL, port, &hints, &addrs);
+  struct addrinfo hints, *listp, *p;
 
-  int listen_fd, optval;
-  for (struct addrinfo *addr = addrs; addr != NULL; addr = addr->ai_next)
-  {
-    listen_fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
-    if (listen_fd == -1)
-      continue;
-    
-    Setsockopt((listen_fd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
-               
-    if (bind(listen_fd, setting->ai_addr, setting->ai_addrlen) == 0)
-               break; 
-    else
-               close(listen_fd);
+  int listenfd, optval = 1;
+
+  /* Получить список потенциальных адресов сервера */
+
+  memset(&hints, 0, sizeof(struct addrinfo));
+
+  hints.ai_socktype = SOCK_STREAM;
+  /* Принимать соединения */
+
+  hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG; /* ... на любом IP-адресе */
+
+  hints.ai_flags |= AI_NUMERICSERV;
+  /* ... номер порта -- число */
+
+  Getaddrinfo(NULL, port, &hints, &listp);
+  /* Найти адрес сокета, который можно связать */
+
+  for (p = listp; p; p = p->ai_next) {
+
+    /* Создать дескриптор сокета */
+
+    if ((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+
+      continue; /* Неудача, попробовать следующий адрес */
+
+    /* Предотвратить появление ошибки "Адрес уже используется" */
+    Setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+
+               (const void *)&optval, sizeof(int));
+
+    /* Связать дескриптор с адресом */
+
+    if (bind(listenfd, p->ai_addr, p->ai_addrlen) == 0)
+
+      break; /* Успех */
+
+    close(listenfd); /* Ошибка, попробовать следующий адрес */
   }
-   
-  Freeaddrinfo(addrs);
-               
-  if (addr == NULL)
+
+  /* Освободить ресурсы */
+
+  Freeaddrinfo(listp);
+
+  if (!p) /* Ни один адрес не подошел */
+
     return -1;
-  
- if (listen(listen_fd, LISTENQ) == -1) 
- {
-   close(listen_fd);
-   return -1; 
- }
- return server_fd; 
+
+  /* Сделать сокет слушающим и готовым принимать запросы на соединение */
+
+  if (listen(listenfd, LISTENQ) < 0) {
+
+    close(listenfd);
+
+    return -1;
+  }
+
+  return listenfd;
+}
+
+int main() {
+  int listenfd, connfd;
+
+  socklen_t clientlen;
+
+  struct sockaddr_storage clientaddr; /* Достаточно места для любого адреса */
+
+  char client_hostname[MAXLINE], client_port[MAXLINE];
+  char *message_to_client = "HTTP/1.1 200 OK\nContent-Type: "
+                            "text/plain\nContent-Length: 12\n\nHello world!";
+
+  listenfd = open_listen_fd("29008");
+
+  while (1) {
+
+    printf("Server waiting\n\n");
+    clientlen = sizeof(struct sockaddr_storage);
+
+    if ((connfd = accept(listenfd, (struct sockaddr *)&clientaddr,
+                         (socklen_t *)&clientlen)) < 0) {
+      perror("In accept");
+      exit(EXIT_FAILURE);
+    }
+
+    // connfd = Accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+
+    Getnameinfo((struct sockaddr *)&clientaddr, clientlen, client_hostname,
+                MAXLINE,
+
+                client_port, MAXLINE, 0);
+
+    printf("Connected to (%s, %s)\n", client_hostname, client_port);
+
+    char buffer[30000] = {0};
+    read(connfd, buffer, 30000);
+    printf("%s\n", buffer);
+    write(connfd, message_to_client, strlen(message_to_client));
+  }
+  exit(0);
 }
