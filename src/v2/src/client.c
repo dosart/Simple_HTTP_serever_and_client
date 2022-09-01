c50213d3d7d34e5deb7585074d0592545220050b
dosart <Dysprosium92@ya.ru>
2022-09-01 19:06:20 +0300

    add implementation of client

--- src/v2/src/client.c
+++ src/v2/src/client.c
@@ -1,5 +1,29 @@
-#include "wrappers.h"
+#include "client.h"
 
-int open_client_fd(char *hostname, char *port) { return 0; }
+int open_client_fd(char *hostname, char *port) {
+  struct addrinfo hints, *results, *p;
 
-int main() { return 0; }
+  memset(&hints, 0, sizeof(struct addrinfo));
+  hints.ai_socktype = SOCK_STREAM; // TCP socket
+  hints.ai_flags = AI_NUMERICSERV; // Port is number
+  hints.ai_flags |= AI_ADDRCONFIG;
+  Getaddrinfo(hostname, port, &hints, &results);
+
+  int client_fd;
+  // Find socket using hints. add socket to list of results
+  for (p = results; p; p = p->ai_next) {
+    if ((client_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+      continue; // Bad luck. Try next address
+
+    if (connect(client_fd, p->ai_addr, p->ai_addrlen) != -1)
+      break; // It's luck.
+
+    close(client_fd); // Bad luck. Close the socket
+  }
+  Freeaddrinfo(results);
+
+  if (p == NULL) // All address is wrong
+    return -1;
+  else
+    return client_fd;
+}
