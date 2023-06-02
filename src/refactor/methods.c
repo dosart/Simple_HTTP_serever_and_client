ef9bb2dee444035b4df6dbac18933e5de52b1b75
dosart <Dysprosium92@ya.ru>
2023-06-02 18:31:13 +0300

    build: select method

--- src/refactor/methods.c
+++ src/refactor/methods.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -81,44 +82,64 @@ void using_fork(int server_fd) {
   }
 }
 
+static void clear(char arr[], int size);
+
 void using_select(int server_fd) {
   printf("Using select() for synchronous multiplexing.\n");
 
   struct sockaddr_storage client_addr;
   socklen_t address_len = sizeof(struct sockaddr_storage);
+  char message_from_client[2048] = {0};
   int client_fd;
 
-  fd_set master;
-  FD_ZERO(&master);
-  FD_SET(server_fd, &master);
-  int nfds = server_fd;
+  fd_set read_set;
+  FD_ZERO(&read_set);
+  FD_SET(server_fd, &read_set);
+  int maxfd = server_fd;
 
   while (1) {
-    fd_set reads;
-    reads = master;
-    if (select(nfds + 1, &reads, NULL, NULL, /* timeout = */ NULL) < 0) {
-      fprintf(stderr, "getaddrinfo() failed. (%d)\n", errno);
+    fd_set ready_set;
+    ready_set = read_set;
+    if (select(maxfd + 1, &ready_set, NULL, NULL, NULL) < 0) {
+      fprintf(stderr, "select failed. (%d)\n", errno);
       exit(EXIT_FAILURE);
     }
-    for (int i = 1; i <= nfds; ++i) {
-      if (FD_ISSET(i, &reads)) {
+    for (int i = 1; i <= maxfd; ++i) {
+      if (FD_ISSET(i, &ready_set)) {
         if (i == server_fd) {
           if ((client_fd = Accept(server_fd, (struct sockaddr *)&client_addr,
                                   (socklen_t *)&address_len)) == -1) {
             exit(EXIT_FAILURE);
           }
-          FD_SET(client_fd, &master);
-          if (client_fd > nfds)
-            nfds = client_fd;
+          FD_SET(client_fd, &read_set);
+          if (client_fd > maxfd)
+            maxfd = client_fd;
         } else {
-          // read from a client socket
-          if (read_and_echo(i)) {
-            // client disconnected
-            FD_CLR(i, &master);
-            close(i);
+          if (Recv(i, message_from_client, 2048, 0) == -1) {
+            exit(EXIT_FAILURE);
           }
+
+          printf("Message from client: %s\n", message_from_client);
+
+          if (Send(i, message_from_client, 2048, 0) == -1) {
+            exit(EXIT_FAILURE);
+          }
+
+          FD_CLR(i, &read_set);
+          close(i);
+          clear(message_from_client, 2048);
         }
       }
     }
+    if (maxfd == FD_SETSIZE) {
+      fprintf(stderr, "too many clients.\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
+static void clear(char arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    arr[i] = '\0';
   }
 }
