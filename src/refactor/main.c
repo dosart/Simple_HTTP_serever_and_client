70ce0cb2b1ccd059858ec969fd2ffc3b4687a79d
dosart <Dysprosium92@ya.ru>
2023-06-03 20:20:54 +0300

    refactor: add file utils.

--- src/refactor/main.c
+++ src/refactor/main.c
@@ -1,23 +1,10 @@
-
-#include "methods.h"
+#include "utils.h"
 #include "wrappers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-static char *make_label(const char **strings, int num_strings);
-
-static const char *method_names[] = {"simple", "fork", "select", "pool",
-                                     "epoll"};
-static int size = sizeof(method_names) / sizeof(method_names[0]);
-
-static int find_method(const char *method, const char *method_names[],
-                       int size);
-
-typedef void (*func_type)(int);
-func_type method_funcs[] = {using_simple, using_fork};
-
 int main(int argc, char const *argv[]) {
-  char *label = make_label(method_names, size);
+  char *label = make_label();
 
   if (argc < 3) {
     printf("Usage: server [port] [%s]\n", label);
@@ -32,49 +19,16 @@ int main(int argc, char const *argv[]) {
     return 1;
   }
 
+  func_type method_func;
   const char *method = argv[2];
-  int method_index = 0;
-  if ((method_index = find_method(method, method_names, size)) == -1) {
+  if ((method_func = find_method(method)) == NULL) {
     printf("Unknown  method. Use  %s.\n", label);
     free(label);
     return 1;
   }
 
   int listen_fd = Open_listen_fd(str_port, 10);
-
-  func_type method_func = method_funcs[method_index];
   method_func(listen_fd);
 
   free(label);
-}
-
-static char *make_label(const char **strings, int num_strings) {
-  int i = 0;
-  unsigned long total_length = 0;
-  char *result;
-
-  // Calculate total length of all strings
-  for (i = 0; i < num_strings; i++) {
-    total_length += strlen(strings[i]);
-  }
-
-  // Allocate memory for result string
-  result = (char *)malloc((total_length + 1) * sizeof(char));
-
-  // Concatenate strings
-  strcpy(result, strings[0]);
-  for (i = 1; i < num_strings; i++) {
-    strcat(result, "|");
-    strcat(result, strings[i]);
-  }
-
-  return result;
-}
-
-static int find_method(const char *method, const char *methods[], int size) {
-  for (int i = 0; i < size; i++) {
-    if (strcmp(methods[i], method) == 0)
-      return i;
-  }
-  return -1;
 }
--- src/refactor/methods.h
+++ src/refactor/methods.h
@@ -1,6 +1,8 @@
 #ifndef METHODS_H
 #define METHODS_H
 
+#include "methods.h"
+
 void using_simple(int server_fd);
 
 void using_fork(int server_fd);
